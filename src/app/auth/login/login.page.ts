56227c83ae071eadd3f532d037c9b677c917f691
ganeshkousik <ganeshkousik313@gmail.com>
2021-10-24 13:11:33 +0530

    navigation

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
+import {FormGroup, FormControl, Validators} from '@angular/forms'
+import { RegisterService } from './../../auth.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,51 @@ import {Router} from '@angular/router'
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private  authService:  RegisterService, private  router:  Router) { }
 
   ngOnInit() {
   }
 
-  redirectRegister(){
-    this.router.navigateByUrl('');
+  loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.minLength(6)])
+  })
+
+  get email(){
+      return this.loginForm.get('email')
   }
+
+  get password(){
+      return this.loginForm.get('password')
+  }
+
+  async onSubmit(){
+    try{
+        // this.authService.saveUser(this.registerForm.value).subscribe((response)=>{
+        //     console.log(response)
+        //     try{
+        //         Swal.fire({
+        //             icon: 'success',
+        //             text: 'User Saved Successfully',
+        //         })
+        //         setTimeout(() => {
+                    this.router.navigate(['home'])
+        //         }, 2000);
+        //     }
+        //     catch(error){
+        //         Swal.fire({
+        //             icon: 'error',
+        //             text: 'User Registration Failed',
+        //         })
+        //     }
+        // })
+    }
+    catch(error){
+        Swal.fire({
+            icon: 'error',
+            text: 'User Login Failed',
+        })
+    }
+}
 
 }
